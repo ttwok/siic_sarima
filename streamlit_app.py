f9f9e504b2e010977caa3c459d86dc2306de1304
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-23 22:08:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -291,8 +291,8 @@ if main_selected_tab == "SIIC Management":
             st.dataframe(df_daily_mall[['날짜','쇼핑몰명','총처리호']])
 
     elif sub_selected_tab == "SIIC 수요예측":
-        st.info("SIIC 콜 처리량 수요예측")
-        st.write('5년간 콜 처리 현황 & M+7 예측값')
+        st.subtitle("SIIC 콜 처리량 수요예측")
+        st.info('5년간 콜 처리 현황 & M+7 예측값')
         call_forecast()
 
 elif main_selected_tab == "SIIC Reporting":
